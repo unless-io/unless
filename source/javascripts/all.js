609abb800ac0101210cded0a1f9d9abb03331283
Lars Bohm <larsbohm@gmail.com>
2016-11-28 17:25:22 +0100

    change location logo to opening text & fixed on scroll

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,3 +1,19 @@
 //= require jquery
 //= require bootstrap-sprockets
 //= require_tree .
+
+$(document).ready(function() {
+
+  $(window).scroll(function () {
+    //if you hard code, then use console
+    //.log to determine when you want the 
+    //nav bar to stick.  
+    console.log($(window).scrollTop())
+    if ($(window).scrollTop() > 300) {
+      $('.nav').removeClass('hidden');
+    }
+    if ($(window).scrollTop() < 301) {
+      $('.nav').addClass('hidden');
+    }
+  });
+});
